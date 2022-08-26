18c5b34bf066bf3f6bc0a6727cf1f64e0bb5686d
Hazuki Kaguya <35645329+HazukiKaguya@users.noreply.github.com>
2022-08-26 19:42:19 +0900

    fix a bug
    fix a bug

--- GuguTown_IconPack.user.js
+++ GuguTown_IconPack.user.js
@@ -2,7 +2,7 @@
 // @name        Gugu Town IconPack
 // @namespace   https://github.com/HazukiKaguya/GuguTown_IconPack
 // @homepage    https://github.com/HazukiKaguya
-// @version     0.2
+// @version     0.2.1
 // @description Gugu Town IconPack
 // @icon        https://sticker.inari.site/favicon.ico
 // @author      Hazuki Kaguya
@@ -13,7 +13,6 @@
 // @require     https://sticker.inari.site/js/jquery.min.user.js
 // @updateURL   https://github.com/HazukiKaguya/GuguTown_IconPack/raw/main/GuguTown_IconPack.user.js
 // ==/UserScript==
-
 /**
  * default settings
  */
@@ -120,7 +119,7 @@ $(".iconpack-usr").click(function(){
 });
 $(".iconpack-switch").click(function(e){ custom.useOldNames = e.target.checked; localStorage.setItem('IconPackConf', JSON.stringify(custom));location.reload();});
 function listener(){console.log("listener fired.");repfunc();}
-document.addEventListener("DOMSubtreeModified", function() { if(timeout) {clearTimeout(timeout);} timeout = setTimeout(listener, 100);}, false);
+document.addEventListener("DOMSubtreeModified", function() { if(timeout) {clearTimeout(timeout);} timeout = setTimeout(listener, 50);}, false);
 function repfunc(){
     if(custom.iconPack=="classic"){ nowIcons=classicIcons;}
     else if(custom.iconPack=="pcr"){ nowIcons=pcrIcons;}
@@ -215,8 +214,9 @@ $('head').append(`<style>
     .btn.fyg_colpzbg.fyg_mp3 { width: ${iconsize} !important; height: ${iconsize} !important; background-size:100% 100%;}
     .img-rounded { width: 50px; height:50px;}
     .btn.fyg_colpzbg.fyg_tc { width: 60px !important; height: 100px !important;line-height:25px;}
+
 </style>`);
-if(custom.iconPack=="pcr"){
-    $('head').append(`<style>
-.fyg_colpz01bg,.fyg_colpz02bg,.fyg_colpz03bg,.fyg_colpz04bg,.fyg_colpz05bg,.fyg_colpz06bg {background-blend-mode: normal !important; }
-</style>`);}
+if(custom.iconPack=="pcr"){$('head').append(`<style>
+[data-trigger=hover] {background-blend-mode: normal !important; }
+</style>`);
+}
